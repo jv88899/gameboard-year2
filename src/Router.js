884b30b8f318dcfc808499b504ca14806fa381bc
jv88899 <jv88899@gmail.com>
2020-10-22 09:56:44 -0500

    created 002, added to router

--- src/Router.js
+++ src/Router.js
@@ -1,22 +1,26 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Transaction001 from './transactions/Transaction001'
+import Transaction001 from "./transactions/Transaction001";
+import Transaction002 from "./transactions/Transaction002";
 
 const Router = () => {
-  const companyName = "Medica"
+  const companyName = "Medica";
 
   return (
     <BrowserRouter>
-    <Switch>
-    <Route path="/" exact>
-      <Transaction001 companyName={companyName} />
-    </Route>
-    <Route path="/001" exact>
-      <Transaction001 companyName={companyName} />
-    </Route>
-    </Switch>
+      <Switch>
+        <Route path="/" exact>
+          <Transaction001 companyName={companyName} />
+        </Route>
+        <Route path="/001" exact>
+          <Transaction001 companyName={companyName} />
+        </Route>
+        <Route path="/002" exact>
+          <Transaction002 companyName={companyName} />
+        </Route>
+      </Switch>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default Router;
