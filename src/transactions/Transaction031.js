d2ef73f7b6919e215057fdaf9e5b2a973b1f1666
jv88899 <jv88899@gmail.com>
2020-10-28 10:06:45 -0500

    created 031, added to router

--- src/transactions/Transaction031.js
+++ src/transactions/Transaction031.js
@@ -160,8 +160,17 @@ const Transaction031 = ({ companyName }) => (
                       <span className="item-total">1</span>
                     </li>
                     <li>
-                      <span className="item-name blue">Accounts Payable</span>
-                      <span className="item-total">2</span>
+                      <Link
+                        to="/032"
+                        style={{
+                          color: "inherit",
+                          cursor: "default",
+                          textDecoration: "none"
+                        }}
+                      >
+                        <span className="item-name blue">Accounts Payable</span>
+                        <span className="item-total">2</span>
+                      </Link>
                     </li>
                     <li>
                       <span className="item-name blue">
