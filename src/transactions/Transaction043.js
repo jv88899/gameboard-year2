057d59774bc645994f30f7f5914483dab6815003
jv88899 <jv88899@gmail.com>
2020-10-28 13:54:02 -0500

    added click handler to 043

--- src/transactions/Transaction043.js
+++ src/transactions/Transaction043.js
@@ -218,8 +218,17 @@ const Transaction043 = ({ companyName }) => (
               <span className="item-total">1</span>
             </li>
             <li>
-              <span className="item-name red">Taxing Authority</span>
-              <span className="item-total">0</span>
+              <Link
+                to="/044"
+                style={{
+                  color: "inherit",
+                  cursor: "default",
+                  textDecoration: "none"
+                }}
+              >
+                <span className="item-name red">Taxing Authority</span>
+                <span className="item-total">0</span>
+              </Link>
             </li>
           </ul>
         </div>
