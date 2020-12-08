94e47e74e1dc06762e50c8fea078f01675233c0a
jv88899 <jv88899@gmail.com>
2020-12-08 12:01:39 -0600

    added click handler to 007

--- src/transactions/Transaction007.js
+++ src/transactions/Transaction007.js
@@ -202,8 +202,17 @@ const Transaction007 = ({ companyName }) => (
               <span className="item-total">0</span>
             </li>
             <li>
-              <span className="item-name red">Medical Providers</span>
-              <span className="item-total">0</span>
+              <Link
+                to="/008"
+                style={{
+                  color: "inherit",
+                  cursor: "default",
+                  textDecoration: "none"
+                }}
+              >
+                <span className="item-name red">Medical Providers</span>
+                <span className="item-total">0</span>
+              </Link>
             </li>
             <li>
               <span className="item-name red">Employees</span>
